fdc227217c8b5bb014679c279ece3937e37021b4
yunuskyya <0yunuskaya0@gmail.com>
2024-04-22 11:07:38 +0300

    ana main classının ismi değiştirildi.

---
+++ src/test/java/com/yunus/security/Ders1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunus.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ders1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
